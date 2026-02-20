058603a00367cc8d03709ff07e3baea78ac86ae3
Sebastian <bastelkaul@gmail.com>
2026-02-20 13:55:56 +0100

    added all files

---
+++ script.js
@@ -0,0 +1,10 @@
+// Animate on scroll
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('[data-animate]').forEach(el => observer.observe(el));
